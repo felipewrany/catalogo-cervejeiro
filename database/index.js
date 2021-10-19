9661a7dacf8d1c5a6f06a5db6af37d7e34ac315c
Felipe Wrany <felipe.wrany@gmail.com>
2021-10-18 23:03:02 -0300

    recuperando o database

---
+++ database/index.js
@@ -0,0 +1,14 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {     
+    dialect: 'postgres',
+    protocol: 'postgres',     
+    dialectOptions: {         
+      ssl: {             
+        require: true,             
+        rejectUnauthorized: false         
+      }     
+    } 
+  }) 
+
+module.exports = sequelize;
