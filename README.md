e8c4ae804014ea3013522e093ba959e02fcb34cd
Felipe Wrany <felipe.wrany@gmail.com>
2021-10-18 22:09:05 -0300

    mudança do banco pra nuvem

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # catalogo-cervejeiro
 Projeto final do módulo 02 do curso da Blue
+Catálogo cervejeiro
+Felipe Wrany e Gabriel Matos
--- database/index.js
+++ database/index.js
@@ -1,9 +1,14 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER,process.env.DB_PASS, {
-    host:  process.env.DB_HOST,
-    port: 5432,
-    dialect: 'postgres'
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL, {     
+    dialect: 'postgres',
+    protocol: 'postgres',     
+    dialectOptions: {         
+      ssl: {             
+        require: true,             
+        rejectUnauthorized: false         
+      }     
+    } 
+  }) 
 
 module.exports = sequelize;
